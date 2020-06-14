5a4042e150c707dd09f316a5263d1c685a864f0b
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-06-14 18:46:34 +0300

    Update main.cpp

--- lib/include/main.cpp
+++ lib/include/main.cpp
@@ -1,19 +1,31 @@
-void solve()
-{
-    
-}
+#ifdef TESTS
+void new_test(){
 
-int32_t main()
-{
-    srand(time(NULL));
+}
+#endif
+
+int32_t main(){
+#ifdef FASTREADWRITE
     ios::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    #ifdef FILE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-    int t;
-    cin >> t;
-    while (t--)
-        solve();
+    cin.tie(nullptr); cout.tie(nullptr);
+#endif
+#ifdef FILE
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+#endif
+#ifdef TESTS
+    int test;
+    cin >> test;
+    while (test--){
+        new_test();
+        cout << "\n";
+    }
+#endif
+#ifndef TESTS
+    
+#endif
+#ifdef FILE
+    fin.close();
+    fout.close();
+#endif
 }
