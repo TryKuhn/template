05828e6a5faba79b93ac1048c178c70160a5016c
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-08 21:23:51 +0300

    Update segment.cpp

--- lib/class/segment tree/segment.cpp
+++ lib/class/segment tree/segment.cpp
@@ -1,92 +1,96 @@
-int64_t s_up(int type, int a, int b){
-    switch (type){
-        case 1:
-            return max(a, b);
-            break;
-        case 2:
-            return min(a, b);
-            break;
-        case 3:
-            return a + b;
-            break;
-        case 4:
-            return a ^ b;
-            break;
-        case 5:
-            return a | b;
-            break;
-        case 6:
-            return a & b;
-            break;
-        case 7:
-            return __gcd(a, b);
-            break;
-        case 8:
-            return a * b;
-            break;
-    }
-    return 0;
-}
-
-class SegmentTree
+class segment
 {
 private:
     static const int N = 1e5;
+    short type = 1;
+    int len = N;
+    array<int, N> a{};
+    array <long long, 4 * N + 4> tree{};
+    long long act(long long f, long long s){
+        switch (type){
+            case 1:
+                return max(f, s);
+                break;
+            case 2:
+                return min(f, s);
+                break;
+            case 3:
+                return f + s;
+                break;
+            case 4:
+                return f ^ s;
+                break;
+            case 5:
+                return f | s;
+                break;
+            case 6:
+                return f & s;
+                break;
+            case 7:
+                return __gcd(f, s);
+                break;
+            case 8:
+                return f * s;
+                break;
+            default:
+                return -1;
+        }
+        return 0;
+    }
+    void do_build(int v, int tl, int tr){
+        if (tl == tr)
+        {
+            tree [v] = a [tl];
+            return;
+        }
+        int tm = (tl + tr) / 2;
+        do_build(v * 2, tl, tm);
+        do_build(v * 2 + 1, tm + 1, tr);
+        tree [v] = act(tree [v * 2], tree [v * 2 + 1]);
+    }
 public:
-    int type;
-    SegmentTree(int type){
-        this->type = type;
+    segment(int tp, int ln){
+        type = tp;
+        len = ln;
     }
-    int n;
-    int64_t tree [4 * N + 1] = {};
-    void tree_build(deque <int> a, int v, int l, int r)
+    template<typename T>
+    void build(T in, int v, int tl, int tr)
     {
-        if (l == r)
+        for(int i = tl; i <= tr; i++)
+            a[i] = in[i];
+        do_build(v, tl, tr);
+    }
+    void update(int v, int tl, int tr, int idx, int new_val)
+    {
+        if (tl == tr)
         {
-            tree [v] = a [l];
+            tree [v] = new_val;
             return;
         }
-        int mid = (l + r) / 2;
-        tree_build(a, v * 2, l, mid);
-        tree_build(a, v * 2 + 1, mid + 1, r);
-        tree [v] = s_up(type, tree [v * 2], tree [v * 2 + 1]);
-    }
-    void tree_build(int a [], int v, int l, int r)
-    {
-        if (l == r)
-        {
-            tree[v] = a[l];
-            return;
-        }
-        int mid = (l + r) / 2;
-        tree_build (a, v * 2, l, mid);
-        tree_build (a, v * 2 + 1, mid + 1, r);
-        tree [v] = s_up(type, tree [v * 2], tree [v * 2 + 1]);
-    }
-    void tree_update(int v, int l, int r, int where, int what)
-    {
-        if (l == r)
-        {
-            tree [v] = what;
-            return;
-        }
-        int mid = (l + r) / 2;
-        if (mid >= where)
-            tree_update(v * 2, l, mid, where, what);
+        int tm = (tl + tr) / 2;
+        if (tm >= idx)
+            update(v * 2, tl, tm, idx, new_val);
         else
-            tree_update(v * 2 + 1, mid + 1, r, where, what);
-        tree [v] = s_up(type, tree [v * 2], tree [v * 2 + 1]);
+            update(v * 2 + 1, tm + 1, tr, idx, new_val);
+        tree [v] = act(tree [v * 2], tree [v * 2 + 1]);
     }
-    int tree_request(int v, int l, int r, int rl, int rr)
-    {
-        if (rl > rr) {
-            if (type == 8)
-                return 1;
-            return 0;
+    long long request(int v, int tl, int tr, int l, int r){
+        if (l > r)
+            return -1;
+        if (tl == l && tr == r)
+            return tree[v];
+        long long ans = LLONG_MAX;
+        int tm = (tl + tr) / 2;
+        int nl = l, nr = min(r, tm);
+        if (nl <= nr)
+            ans = request(v * 2, tl, tm, nl, nr);
+        nl = max(l, tm + 1), nr = r;
+        if (nl <= nr){
+            if (ans != LLONG_MAX)
+                ans = act(ans, request(v * 2 + 1, tm + 1, tr, nl, nr));
+            else
+                ans = request(v * 2 + 1, tm + 1, tr, nl, nr);
         }
-        if (l == rl && r == rr)
-            return tree [v];
-        int mid = (l + r) / 2;
-        return s_up(type, tree_request(v * 2, l, mid, rl, min(mid, rr)), tree_request(v * 2 + 1, mid + 1, r, max(mid + 1, rl), rr));
+        return ans;
     }
 };
