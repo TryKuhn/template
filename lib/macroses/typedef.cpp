a33a59ac76c6887bde2c2c55f90cec79c585343f
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-11 19:20:41 +0300

    Update typedef.cpp

--- lib/macroses/typedef.cpp
+++ lib/macroses/typedef.cpp
@@ -1,10 +1,7 @@
 typedef long long ll;
-typedef pair <int, int> pii;
-typedef vector <int> vi;
-typedef deque <int> di;
-typedef deque <pii> dii;
-typedef map <int, di, greater <int> > mii;
-typedef set <int> si;
-typedef multiset <int> mi;
-typedef string st;
-typedef priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap_Dijkstra;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef deque<int> di;
+typedef map<int, int> mii;
+typedef set<int> si;
+typedef priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> heap_dijkstra;
