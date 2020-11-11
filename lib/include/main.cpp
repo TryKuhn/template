2c6f5f9f1297a05d7bcf19dd7f45aeb9a0272e85
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-11 20:46:22 +0300

    Update main.cpp

--- lib/include/main.cpp
+++ lib/include/main.cpp
@@ -1,9 +1,3 @@
-#ifdef TESTS
-void new_test(){
-
-}
-#endif
-
 int32_t main(){
 #ifdef FASTREADWRITE
     ios::sync_with_stdio(false);
@@ -13,17 +7,10 @@ int32_t main(){
     ifstream fin("input.txt");
     ofstream fout("output.txt");
 #endif
-#ifdef TESTS
     int test;
     cin >> test;
-    while (test--){
+    while (test--)
         new_test();
-        cout << "\n";
-    }
-#endif
-#ifndef TESTS
-    
-#endif
 #ifdef FILE
     fin.close();
     fout.close();
