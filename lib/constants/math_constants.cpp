3a5bc42e1a534fbabac884e0cf517ace3a2d4e7f
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-11 19:15:49 +0300

    Update math_constants.cpp

--- lib/constants/math_constants.cpp
+++ lib/constants/math_constants.cpp
@@ -1,5 +1,3 @@
-const int LEN_ = 1e6;
-const int mod = 1e9 + 7;
 const double pi = 3.14159265359;
 const double fi = 1.61803398875;
-const double e = 2.718281828459;
+const double e =  2.718281828459;
