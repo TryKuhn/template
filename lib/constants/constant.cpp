d0f45b28a4846a4bc87c497d85c3774c0df08078
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-11 19:08:26 +0300

    Update and rename constant.cpp to math_constant.cpp

--- lib/constants/constant.cpp
+++
@@ -1,8 +0,0 @@
-const int N = 1e6;
-const int64_t MAXL = LLONG_MAX;
-const int64_t MINL = LLONG_MIN;
-const int32_t MAXI = INT_MAX;
-const int32_t MINI = INT_MIN;
-const int MOD = 1e9 + 7;
-const double pi = 3.14159265359;
-const double g = 9.81;
---
+++ lib/constants/math_constant.cpp
@@ -0,0 +1,5 @@
+const int LEN_ = 1e6;
+const int mod = 1e9 + 7;
+const double pi = 3.14159265359;
+const double fi = 1.61803398875;
+const double e = 2.718281828459;
