b442375b44afa7b243f8b4b3d4d4132015649b19
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-13 08:33:33 +0300

    Update define.cpp

--- lib/macroses/define.cpp
+++ lib/macroses/define.cpp
@@ -18,3 +18,13 @@
 #ifdef LONG
     #define int long long
 #endif
+
+#ifdef MATH
+    #define INFD 1.0 / 0
+    #define pi acos(-1)
+    #define MOD (int)(1e9 + 7)
+    #define sinDegrees(x) sin((x) * PI / 180.0)
+    #define cosDegrees(x) cos((x) * PI / 180.0)
+    #define tanDegrees(x) tan((x) * PI / 180.0)
+    #define atanDegrees(x) atan(x)* 180.0 / PI
+#endif
