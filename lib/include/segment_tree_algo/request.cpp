b4b775c9abbbaa2e5a7ff33e0adb67a233483500
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-13 08:43:22 +0300

    Update request.cpp

--- lib/include/segment_tree_algo/request.cpp
+++ lib/include/segment_tree_algo/request.cpp
@@ -4,6 +4,6 @@ long long get(int v, int l, int r, int tl, int tr)
         return 0;
     if (l == tl && r == tr)
         return tree [v];
-    int mid = (l + r) / 2;
+    int tm = (tl + tr) / 2;
     return get(v * 2, l, min(r, tm), tl, tm) + get(v * 2 + 1, max(l, tm + 1), r, tm + 1, tr);
 }
