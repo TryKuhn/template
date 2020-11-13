2cc3edf5165f9f6b7c15b9c48ebd7d72732c46f6
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-13 10:04:24 +0300

    Update merge.cpp

--- lib/include/sort_algo/merge_sort/merge.cpp
+++ lib/include/sort_algo/merge_sort/merge.cpp
@@ -1,32 +1,34 @@
-deque <int> merge_sort(deque <int> a1, deque <int> a2)
+template <typename T, typename K>
+T merge_sort(T& a1, K& a2)
 {
-    deque <int> a3 (a1.size() + a2.size());
+    T a3 (a1.size() + a2.size());
     int i = 0, j = 0;
-    for (int k = 0; k < a3.size(); k++) 
+    for (int k = 0; k < a3.size(); k++)
     {
-        if (i >= a1.size()) 
+        if (i >= a1.size())
         {
             int a = a2[j];
             a3[k] = a;
             j++;
         }
-        else if (j >= a2.size()) 
+        else if (j >= a2.size())
         {
             int a = a1[i];
             a3[k] = a;
             i++;
         }
-        else if (a1[i] < a2[j]) 
+        else if (a1[i] < a2[j])
         {
-            int a = a1[i]; 
+            int a = a1[i];
             a3[k] = a;
             i++;
         }
-        else 
+        else
         {
             int b = a2[j];
             a3[k] = b;
             j++;
         }
     }
+    return a3;
 }
