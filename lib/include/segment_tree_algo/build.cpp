0c1d11e55df299bfcf8ef4bbf279d980e6d44aa9
Egor Anisevich <65305629+GitEgorHub@users.noreply.github.com>
2020-11-13 08:38:13 +0300

    Update build.cpp

--- lib/include/segment_tree_algo/build.cpp
+++ lib/include/segment_tree_algo/build.cpp
@@ -1,4 +1,5 @@
-void tree_build(di a, int v, int l, int r)
+template<typename T>
+void tree_build(T& a, int v, int l, int r)
 {
     if (l == r)
     {
@@ -8,17 +9,5 @@ void tree_build(di a, int v, int l, int r)
     int mid = (l + r) / 2;
     tree_build(a, v * 2, l, mid);
     tree_build(a, v * 2 + 1, mid + 1, r);
-    tree [v] = tree [v * 2] + tree [v * 2 + 1];
-}
-void tree_build(int a [], int v, int l, int r)
-{
-    if (l == r)
-    {
-        tree[v] = a[l];
-        return;
-    }
-    int mid = (l + r) / 2;
-    tree_build (a, v * 2, l, mid);
-    tree_build (a, v * 2 + 1, mid + 1, r);
     tree [v] = tree [v * 2] + tree [v * 2 + 1];
 }
